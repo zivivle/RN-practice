246b5ab936437489536aeae618e4649670dd1ad0
zivivle <123868471+zivivle@users.noreply.github.com>
2024-05-22 17:11:23 +0900

    feat: 기본 요소 사용 test

--- zivivle/app/(tabs)/index.tsx
+++ zivivle/app/(tabs)/index.tsx
@@ -1,26 +1,25 @@
-import { Image, ScrollView, Text, View,TextInput  } from 'react-native';
+import { Text, View, StyleSheet } from "react-native";
+
 
 export default function HomeScreen() {
   return (
-    <ScrollView>
-      <Text>Some text</Text>
-      <View>
-        <Text>Some more text</Text>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{width: 200, height: 200}}
-        />
-      </View>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
-        defaultValue="You can type in me"
-      />
-    </ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Hello World!
+      </Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  }
+})
