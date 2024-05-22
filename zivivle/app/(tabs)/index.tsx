b45044e0d30d55117c268b7596cfb0966e1d4da9
zivivle <123868471+zivivle@users.noreply.github.com>
2024-05-22 18:13:31 +0900

    feat: 기본 useState 적용 테스트

--- zivivle/app/(tabs)/index.tsx
+++ zivivle/app/(tabs)/index.tsx
@@ -1,90 +1,27 @@
+import { useState } from "react";
 import { Text, View, StyleSheet, Image, TextInput, ScrollView, Button, Switch } from "react-native";
 
 
 export default function HomeScreen() {
+  const [value, setValue] = useState(false);
+  const [text, setText] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Hello World!
-      </Text>
-      {/* local에서 이미지를 가져오는 방법 */}
-      <Image 
-        source={require('../../assets/images/favicon.png')}
-        style={styles.icon_image}
-      />
-      {/* 외부 경로 이미지를 가져오는 방법 */}
-      <Image 
-        source={{uri: "https://reactnative.dev/docs/assets/p_cat2.png"}}
-        style={styles.icon_image}
-      />
-
-      <TextInput placeholder="이름을 입력해주세요." />
-
-      {/* <ScrollView>
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        
-        <Image 
-          source={require('../../assets/images/favicon.png')}
-          style={styles.icon_image}
-        />
-        
-      </ScrollView> */}
       <Button title="Click Me!" onPress={() => {
         console.log("버튼 클릭!");
       }}/>
 
-      <Switch value={true} />
-      <Switch value={false} />
+      <Switch value={value} onValueChange={v => {
+        setValue(v)
+      }}/>
+      <TextInput 
+        value={text} 
+        onChangeText={v => {
+          setText(v)
+        }}
+        style={{ backgroundColor: "pink", width: "100%" }}
+      />
     </View>
   );
 }
