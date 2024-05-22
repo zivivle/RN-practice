a073d18cf739bfce54442e91d0526dd0ed1caed3
zivivle <123868471+zivivle@users.noreply.github.com>
2024-05-22 17:21:34 +0900

    feat: 스크롤뷰 테스트

--- zivivle/app/(tabs)/index.tsx
+++ zivivle/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image, TextInput, ScrollView } from "react-native";
 
 
 export default function HomeScreen() {
@@ -7,6 +7,78 @@ export default function HomeScreen() {
       <Text style={styles.text}>
         Hello World!
       </Text>
+      {/* local에서 이미지를 가져오는 방법 */}
+      <Image 
+        source={require('../../assets/images/favicon.png')}
+        style={styles.icon_image}
+      />
+      {/* 외부 경로 이미지를 가져오는 방법 */}
+      <Image 
+        source={{uri: "https://reactnative.dev/docs/assets/p_cat2.png"}}
+        style={styles.icon_image}
+      />
+
+      <TextInput placeholder="이름을 입력해주세요." />
+
+      <ScrollView>
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        
+        <Image 
+          source={require('../../assets/images/favicon.png')}
+          style={styles.icon_image}
+        />
+        
+      </ScrollView>
     </View>
   );
 }
@@ -21,5 +93,9 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  icon_image: {
+    width: 50,
+    height: 50,
   }
 })
